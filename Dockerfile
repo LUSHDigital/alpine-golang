fd6ba56a0561b65c75cebf74e56bc495a47d80b3
zeeraw <zee@vall.in>
2019-09-25 14:24:47 +0200

    fix: make sure go build using the current revision as build flag

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,14 @@ RUN apk update && \
         tzdata \
         git
 
+ARG APPBIN="build/service"
 ARG GOTARGET="service/*.go"
 ARG GOMODPROXY
 
 ENV CGO_ENABLED="0"
 ENV GOPROXY=${GOMODPROXY}
 ENV GOTARGET=${GOTARGET}
+ENV APPBIN=${APPBIN}
 
 # The build script contains everything you need for building the service/ directory in your project.
 COPY build.sh /build.sh
--- build.sh
+++ build.sh
@@ -5,12 +5,14 @@ LATEST_TAG_COMMIT=$(git rev-list --tags --max-count=1)
 LATEST_TAG=$(git describe --tags ${LATEST_TAG_COMMIT} 2>/dev/null)
 
 go build \
-    -o build/service \
+    -o ${APPBIN} \
     -mod=vendor \
     -ldflags "
         -X main.ref=${CURRENT_REVISION}
         -X main.tag=${LATEST_TAG}
-        -X github.com/LUSHDigital/core.ref=${LATEST_TAG}
+        -X github.com/LUSHDigital/core.ref=${CURRENT_REVISION}
         -X github.com/LUSHDigital/core.tag=${LATEST_TAG}
     " \
     ${GOTARGET}
+
+upx -q ${APPBIN}
